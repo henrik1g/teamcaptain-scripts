b51435e5a70051a4ae1e65ea2f9fd1edc1450900
flyingjoe <johannes@dibberns.com>
2023-08-02 14:14:53 +0200

    update

---
+++ converter.py
@@ -0,0 +1,36 @@
+import csv
+import json
+
+for file in ["club.txt", "std.txt"]:
+    lines = []
+    # read as csv with ID,CALL,CN,TYPE,NAME
+    with open(file, "r") as f:
+        reader = csv.reader(f, delimiter="\t")
+        for row in reader:
+            lines.append(row[0].split(","))
+        # write as json with structure like
+        # [
+        #     {
+        #         "name": "John Smith",
+        #         "cn": "123",
+        #         "glider": "K6e",
+        #         "comp": "Club Class",
+        #         "flarm": ["DDEEFF"]
+        #     },
+        # ]
+        with open(file.replace(".txt", ".json"), "w") as f:
+            json.dump(
+                [
+                    {
+                        "name": line[4] if len(line) > 4 else "",
+                        "cn": line[2] if len(line) > 2 else "",
+                        "glider": "",
+                        "comp": line[2] if len(line) > 2 else "",
+                        "flarm": [line[0]] if len(line) > 0 else "",
+                    }
+                    for line in lines[1:]
+                ],
+                f,
+                indent=4,
+            )
+        
